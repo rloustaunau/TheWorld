524911a1a7f6fc72bdaac80cb082b917cb0219d2
e137239 <e137239@HJZKWV1>
2016-12-03 12:21:49 -0600

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,40 @@
+﻿/* site.js */
+
+(function () {
+
+   
+    //var ele = $("#username");
+    //                // using old javascript ; cross browser functionality - var ele = document.getElementById("username");
+    //ele.text('Rafael Loustaunau2');
+
+    //                // var main = document.getElementById("main");
+    //var main = $('#main');
+
+    //main.on('mouseenter', function () {
+    //    $("#main").css("background-color", "blue");       
+    //})
+
+    //main.on('mouseleave', function () {
+    //    $("#main").css("background-color", "");     
+    //})
+
+
+    //var menuItems = $('ul.menu li a');
+
+    //menuItems.on('click', function () {
+    //    alert('hello');
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+    
+})();
+
